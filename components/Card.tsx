526c39f515175e14642848ded1c28fe29a4d9447
unknown <raphael.malburg@gmail.com>
2023-07-20 09:53:36 +0100

    point in time to the cart issue

--- components/Card.tsx
+++ components/Card.tsx
@@ -8,13 +8,15 @@ interface CardProps {
   price: number | string;
   image: string;
   value?: number;
+  inCart?: number;
 }
-const Card = ({ title, description, price, image, value }: CardProps) => {
+const Card = ({ title, description, price, image, value, inCart }: CardProps) => {
   const { addItemsIntoCart } = useCart();
 
   return (
     <div className=" bg-white border border-gray-200 rounded-lg shadow mr-10 flex flex-col justify-center align-middle dark:bg-gray-800 dark:border-gray-700 min-h-full relative -z-10">
-      <span className="absolute top-1 left-1 rounded-full w-7 flex justify-center align-middle bg-amber-400 text-lg text-pink-950 font-bold z-10">1</span>
+      {inCart && <span className="absolute top-1 left-1 rounded-full w-7 flex justify-center align-middle bg-amber-400 text-lg text-pink-950 font-bold z-10">{inCart}</span>}
+
       <Image className="rounded-t-lg max-h-60 object-cover" src={image} alt="" />
 
       <div className="p-5  h-fit">
@@ -31,9 +33,9 @@ const Card = ({ title, description, price, image, value }: CardProps) => {
             Add to cart
             <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clip-rule="evenodd"></path>
+                clipRule="evenodd"></path>
             </svg>
           </button>
         </div>
--- components/GridCard.tsx
+++ components/GridCard.tsx
@@ -4,6 +4,7 @@ import { motion } from "framer-motion";
 import { Product } from "../content/products";
 import Card from "./Card";
 import { useProductContext } from "@/context/ProductContext";
+import { useCart } from "@/hooks/useCart";
 
 interface GridCardProps {
   children: Product[];
@@ -24,13 +25,17 @@ const GridCard: React.FC<GridCardProps> = ({ children }) => {
     hidden: { opacity: 0, scale: 0 },
     visible: { opacity: 1, scale: 1 },
   };
+  const { cart } = useCart();
   return (
     <motion.div className="grid_product gap-14 content-center justify-items-center align-content-center " variants={gridVariants} initial="hidden" animate="visible">
-      {children.map((item: Product, index: number) => (
-        <motion.div key={index} variants={cardVariants}>
-          <Card title={item.title} description={item.description} price={item.price} image={item.image} value={item.price} />
-        </motion.div>
-      ))}
+      {children.map((item: Product, index: number) => {
+        const inCart = cart.find((cartItem) => cartItem.title === item.title)?.quantity;
+        return (
+          <motion.div key={index} variants={cardVariants}>
+            <Card title={item.title} description={item.description} price={item.price} image={item.image} value={item.price} inCart={inCart} />
+          </motion.div>
+        );
+      })}
     </motion.div>
   );
 };
